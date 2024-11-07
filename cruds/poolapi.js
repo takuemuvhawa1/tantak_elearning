5ecca48b308b42ceecb47765582ee22a7cc1c89e
takuemuvhawa1 <takuemuvhawa@gmail.com>
2024-11-07 05:51:19 +0200

    Update poolapi.js

--- cruds/poolapi.js
+++ cruds/poolapi.js
@@ -1,4 +1,4 @@
-const pool = 'http://localhost:3003';
-// const pool = 'https://nlcc-backend-1.onrender.com';
+// const pool = 'http://localhost:3003';
+const pool = 'https://tantak-elearning.onrender.com';
 
 module.exports = pool; 
